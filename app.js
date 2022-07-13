27dbdbbe34baa50bf77ce50486acfb725522caf5
Nau-c <nauzetlm90@hotmail.com>
2022-07-13 19:44:17 +0100

    Arrancamos el proyecto comenzamos login en VUE

---
+++ app.js
@@ -0,0 +1,12 @@
+//Instaciamos objeto y le pasamos el parametro
+new Vue({
+    el: '#app',
+//Creamos la funcion para obtener los datos
+    data() {
+        return {
+            name: 'loggin!',
+            img: 'img/loggin.jpg'
+        }
+    }
+
+}) 
